6ec4d9c881e27a943c34dd99cbd5c31fe7a72de2
Jillian <xie.ljing@gmail.com>
2023-10-04 15:41:15 -0400

    bug fixed

--- A1/importYUV.m
+++ A1/importYUV.m
@@ -3,16 +3,16 @@ function [Y,U,V] = importYUV(yuvFileName, width, height, nFrame)
     stream = fread(fid,'*uchar');   
     length = 1.5 * width * height;  
 
-    Y = uint8(zeros(height,   width,   nFrame));
-    U = uint8(zeros(height/2, width/2, nFrame));
-    V = uint8(zeros(height/2, width/2, nFrame));
+    Y = uint8(zeros(width, height, nFrame));
+    U = uint8(zeros(width/2, height/2, nFrame));
+    V = uint8(zeros(width/2, height/2, nFrame));
 
     for iFrame = 1:nFrame
         frame = stream((iFrame-1)*length+1:iFrame*length);
 
-        yImage = reshape(frame(1:width*height), width, height)';
-        uImage = reshape(frame(width*height+1:1.25*width*height), width/2, height/2)';
-        vImage = reshape(frame(1.25*width*height+1:1.5*width*height), width/2, height/2)';
+        yImage = reshape(frame(1:width*height), width, height);
+        uImage = reshape(frame(width*height+1:1.25*width*height), width/2, height/2);
+        vImage = reshape(frame(1.25*width*height+1:1.5*width*height), width/2, height/2);
 
         Y(:,:,iFrame) = uint8(yImage);
         U(:,:,iFrame) = uint8(uImage);
---
+++ A1/scaleYUV420To444.m
@@ -0,0 +1,26 @@
+function [Yscale,Uscale,Vscale] = scaleYUV420To444(Y, U, V, width, height ,nFrame)
+    Yscale = uint8(Y);
+    Uscale = uint8(zeros(width, height, nFrame));
+    Vscale = uint8(zeros(width, height, nFrame));
+
+    for iFrame = 1:nFrame
+        for i = 1:width/2
+            for j = 1:height/2
+                Uval = U(i,j,iFrame);
+                Vval = V(i,j,iFrame);
+                ii = 1+(i-1)*2;
+                jj = 1+(j-1)*2;
+
+                Uscale(ii,jj+1,iFrame) = Uval;
+                Uscale(ii+1,jj,iFrame) = Uval;
+                Uscale(ii+1,jj+1,iFrame) = Uval;
+                Uscale(ii,jj,iFrame) = Uval;
+
+                Vscale(ii,jj+1,iFrame) = Vval;
+                Vscale(ii+1,jj,iFrame) = Vval;
+                Vscale(ii+1,jj+1,iFrame) = Vval;
+                Vscale(ii,jj,iFrame) = Vval;
+            end
+        end
+    end
+end
