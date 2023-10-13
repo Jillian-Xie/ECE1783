550d718c1f8a1430b296468d24014c03a90313e7
Shengxiang Ji <shengxiang.ji123@gmail.com>
2023-10-13 00:32:29 -0400

    update padding values to 128

--- A1/paddingFrames.m
+++ A1/paddingFrames.m
@@ -6,10 +6,10 @@ function frames = paddingFrames(Y, blockSize, width, height, nFrame)
     frames(1:width,1:height,:) = Y;
     
     if(rem(width,blockSize)~=0)
-        frames(width+1:horizontal*blockSize,:)=uint8(127);
+        frames(width+1:horizontal*blockSize,:)=uint8(128);
     end
     if(rem(height,blockSize)~=0)
-        frames(:,height+1:vertical*blockSize,:)=uint8(127);
+        frames(:,height+1:vertical*blockSize,:)=uint8(128);
     end
     
 end
