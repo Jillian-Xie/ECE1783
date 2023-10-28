bbe2d1a73fdc5b8186b80d69b7df29e8f25b4a37
Shengxiang Ji <shengxiang.ji123@gmail.com>
2023-10-28 14:05:21 -0400

    add plots for per-frame PSNR and per-frame average MAE

--- A1/run_ex3.m
+++ A1/run_ex3.m
@@ -6,16 +6,24 @@ height = uint32(288);
 nFrame = uint32(10);
 x_frame = [1:nFrame];
 
-% varyBlockSizes(yuvInputFileName, width, height, nFrame, x_frame);
-% varyN(yuvInputFileName, width, height, nFrame, x_frame);
-varyR(yuvInputFileName, width, height, nFrame, x_frame);
+yuvInputFileNameSeparator = split(yuvInputFileName, '.');
+plotOutputPath = strcat('ex3_', yuvInputFileNameSeparator{1,1}, '_Plots', filesep);
 
-function varyBlockSizes(yuvInputFileName, width, height, nFrame, x_frame)
+if ~exist(plotOutputPath,'dir')
+    mkdir(plotOutputPath)
+end
+
+varyBlockSizes(yuvInputFileName, width, height, nFrame, x_frame, plotOutputPath);
+varyN(yuvInputFileName, width, height, nFrame, x_frame, plotOutputPath);
+varyR(yuvInputFileName, width, height, nFrame, x_frame, plotOutputPath);
+
+function varyBlockSizes(yuvInputFileName, width, height, nFrame, x_frame, plotOutputPath)
     r = 4;
     n = 3;
     blockSizes = [2, 8, 64];
     PSNRs = zeros(size(blockSizes, 2), nFrame);
     MAEs = zeros(size(blockSizes, 2), nFrame);
+    legends = strings(1, size(blockSizes, 2));
     for i = 1:size(blockSizes, 2)
         blockSize = blockSizes(1, i);
         [Y, reconstructedYFrame, avgMAE] = ex3(yuvInputFileName, nFrame, width, height, blockSize, r, n);
@@ -24,22 +32,30 @@ function varyBlockSizes(yuvInputFileName, width, height, nFrame, x_frame)
             PSNRs(i, j) = PSNRFrame;
         end
         MAEs(i, :) = avgMAE;
+        legends(1, i) = strcat('i=', num2str(blockSizes(1, i)));
     end
-    figure
-    for i = 1:size(blockSizes, 2)
-        plot(x_frame,PSNRs(i, :),'DisplayName',strcat('i=', num2str(blockSizes(1, i))));
-        hold on
-    end
-    legend('location', 'best');
-    hold off
+    
+    xaxisLabel = 'Frame';
+    yaxisLabel = 'PSNR';
+    titleStr = strcat('PSNR when r=', num2str(r), ', n=', num2str(n), ' and varying i');
+    filenameStr = strcat('psnr_r_', num2str(r), '_n_', num2str(n), '_varying_i.jpeg');
+    plotAgainstFrame(x_frame, PSNRs, legends, xaxisLabel, yaxisLabel, titleStr, filenameStr, plotOutputPath);
+    
+    
+    xaxisLabel = 'Frame';
+    yaxisLabel = 'MAE';
+    titleStr = strcat('MAE when r=', num2str(r), ', n=', num2str(n), ' and varying i');
+    filenameStr = strcat('mae_r_', num2str(r), '_n_', num2str(n), '_varying_i.jpeg');
+    plotAgainstFrame(x_frame, MAEs, legends, xaxisLabel, yaxisLabel, titleStr, filenameStr, plotOutputPath);
 end
 
-function varyN(yuvInputFileName, width, height, nFrame, x_frame)
+function varyN(yuvInputFileName, width, height, nFrame, x_frame, plotOutputPath)
     r = 4;
     blockSize = 8;
     ns = [1, 2, 3];
     PSNRs = zeros(size(ns, 2), nFrame);
     MAEs = zeros(size(ns, 2), nFrame);
+    legends = strings(1, size(ns, 2));
     for i = 1:size(ns, 2)
         n = ns(1, i);
         [Y, reconstructedYFrame, avgMAE] = ex3(yuvInputFileName, nFrame, width, height, blockSize, r, n);
@@ -48,22 +64,30 @@ function varyN(yuvInputFileName, width, height, nFrame, x_frame)
             PSNRs(i, j) = PSNRFrame;
         end
         MAEs(i, :) = avgMAE;
+        legends(1, i) = strcat('n=', num2str(ns(1, i)));
     end
-    figure
-    for i = 1:size(ns, 2)
-        plot(x_frame,PSNRs(i, :),'DisplayName',strcat('n=', num2str(ns(1, i))));
-        hold on
-    end
-    legend('location', 'best');
-    hold off
+    
+    xaxisLabel = 'Frame';
+    yaxisLabel = 'PSNR';
+    titleStr = strcat('PSNR when r=', num2str(r), ', i=', num2str(blockSize), ' and varying n');
+    filenameStr = strcat('psnr_r_', num2str(r), '_i_', num2str(blockSize), '_varying_n.jpeg');
+    plotAgainstFrame(x_frame, PSNRs, legends, xaxisLabel, yaxisLabel, titleStr, filenameStr, plotOutputPath);
+    
+    
+    xaxisLabel = 'Frame';
+    yaxisLabel = 'MAE';
+    titleStr = strcat('MAE when r=', num2str(r), ', i=', num2str(blockSize), ' and varying n');
+    filenameStr = strcat('mae_r_', num2str(r), '_i_', num2str(blockSize), '_varying_n.jpeg');
+    plotAgainstFrame(x_frame, MAEs, legends, xaxisLabel, yaxisLabel, titleStr, filenameStr, plotOutputPath);
 end
 
-function varyR(yuvInputFileName, width, height, nFrame, x_frame)
+function varyR(yuvInputFileName, width, height, nFrame, x_frame, plotOutputPath)
     n = 3;
     blockSize = 8;
     rs = [1, 4, 8];
     PSNRs = zeros(size(rs, 2), nFrame);
     MAEs = zeros(size(rs, 2), nFrame);
+    legends = strings(1, size(rs, 2));
     for i = 1:size(rs, 2)
         r = rs(1, i);
         [Y, reconstructedYFrame, avgMAE] = ex3(yuvInputFileName, nFrame, width, height, blockSize, r, n);
@@ -72,12 +96,38 @@ function varyR(yuvInputFileName, width, height, nFrame, x_frame)
             PSNRs(i, j) = PSNRFrame;
         end
         MAEs(i, :) = avgMAE;
+        legends(1, i) = strcat('r=', num2str(rs(1, i)));
     end
+    
+    xaxisLabel = 'Frame';
+    yaxisLabel = 'PSNR';
+    titleStr = strcat('PSNR when i=', num2str(blockSize), ', n=', num2str(n), ' and varying r');
+    filenameStr = strcat('psnr_i_', num2str(blockSize), '_n_', num2str(n), '_varying_r.jpeg');
+    plotAgainstFrame(x_frame, PSNRs, legends, xaxisLabel, yaxisLabel, titleStr, filenameStr, plotOutputPath);
+    
+    
+    xaxisLabel = 'Frame';
+    yaxisLabel = 'MAE';
+    titleStr = strcat('MAE when i=', num2str(blockSize), ', n=', num2str(n), ' and varying r');
+    filenameStr = strcat('mae_i_', num2str(blockSize), '_n_', num2str(n), '_varying_r.jpeg');
+    plotAgainstFrame(x_frame, MAEs, legends, xaxisLabel, yaxisLabel, titleStr, filenameStr, plotOutputPath);
+end
+
+
+
+function plotAgainstFrame(x_frame, yVals, legends, xaxisLabel, yaxisLabel, titleStr, filenameStr, plotOutputPath)
     figure
-    for i = 1:size(rs, 2)
-        plot(x_frame,PSNRs(i, :),'DisplayName',strcat('r=', num2str(rs(1, i))));
+    for i = 1:size(yVals, 1)
+        plot(x_frame, yVals(i, :),'DisplayName',legends(1, i));
         hold on
     end
     legend('location', 'best');
+    xlabel(xaxisLabel);
+    ylabel(yaxisLabel);
+    grid on;
     hold off
+    
+    title(titleStr);
+    saveas(gcf, fullfile(strcat(plotOutputPath, filenameStr)));
+    delete(gcf);
 end
