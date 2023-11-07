1e8604efbb6a162e84e1275830da8060cc37fb2d
Jillian <xie.ljing@gmail.com>
2023-11-06 17:54:33 -0800

    update

--- A2/interPredictBlock.m
+++ A2/interPredictBlock.m
@@ -1,46 +1,14 @@
 function [bestMV, quantizedBlock, reconstructedBlock] = interPredictBlock(refFrame, currentFrame, widthBlockIndex, heightBlockIndex,r,blockSize, QP, VBSEnable, FMEEnable, FastME)
-bestMAE = Inf;
-bestMV = int32([0, 0, 0]);
-currentBlock = getBlockContent(widthBlockIndex, heightBlockIndex, blockSize, currentFrame,0,0);
-residualBlock = int32(currentBlock);
 
-numRefFrames = size(refFrame,3);
-for indexRefFrame = 1:numRefFrames
-    for mvX = -r:r
-        for mvY = -r:r
-            refWidthPixelIndex = int32((int32(widthBlockIndex)-1)*blockSize + 1 + mvX);
-            refHeightPixelIndex = int32((int32(heightBlockIndex)-1)*blockSize + 1 + mvY);
-            if checkFrameBoundary(refWidthPixelIndex, refHeightPixelIndex, blockSize, refFrame(:,:,indexRefFrame)) == 1
-                refBlock = getBlockContent(widthBlockIndex, heightBlockIndex, blockSize, refFrame(:,:,indexRefFrame), mvX, mvY);
-                mae = sum(abs(int32(currentBlock) - int32(refBlock)), "all") / numel(currentBlock);
-                if mae < bestMAE % Update bestMV
-                    bestMAE = mae;
-                    bestMV = [mvX, mvY, indexRefFrame - 1];
-                    residualBlock = int32(currentBlock) - int32(refBlock);
-                    referenceBlock = int32(refBlock);
-                elseif mae == bestMAE % If there is still a tie, choose the block with smallest 𝑦
-                    currentL1Norm = abs(mvX) + abs(mvY);
-                    bestL1Norm = abs(bestMV(1)) + abs(bestMV(2));
-                    if currentL1Norm < bestL1Norm
-                        bestMV = [mvX, mvY, indexRefFrame - 1];
-                        residualBlock = int32(currentBlock) - int32(refBlock);
-                        referenceBlock = int32(refBlock);
-                    elseif currentL1Norm == bestL1Norm % choose the one with the smallest 𝑥
-                        if mvY < bestMV(2)
-                            bestMV = [mvX, mvY, indexRefFrame - 1];
-                            residualBlock = int32(currentBlock) - int32(refBlock);
-                            referenceBlock = int32(refBlock);
-                        elseif mvY == bestMV(2) && mvX < bestMV(1)
-                            bestMV = [mvX, mvY, indexRefFrame - 1];
-                            residualBlock = int32(currentBlock) - int32(refBlock);
-                            referenceBlock = int32(refBlock);
-                        end
-                    end
-                end
-            end
-        end
-    end
+widthPixelIndex = int32((int32(widthBlockIndex)-1)*blockSize + 1);
+heightPixelIndex = int32((int32(heightBlockIndex)-1)*blockSize + 1);
+
+if FastME
+    [bestMV, referenceBlock, residualBlock] = integerPixelFullSearch(refFrame, currentFrame, widthPixelIndex, heightPixelIndex, blockSize, r);
+else
+    [bestMV, referenceBlock, residualBlock] = fastMotionEstimation(refFrame, currentFrame, widthPixelIndex, heightPixelIndex, blockSize, r);
 end
+
 bestMV = int32(bestMV);
 transformedBlock = dct2(residualBlock);
 quantizedBlock = quantize(transformedBlock, QP);
