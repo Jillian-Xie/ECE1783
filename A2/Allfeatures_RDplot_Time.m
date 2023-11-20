9fb0b8f08f3767bffa2cda23f350c764753d5216
ginnezhang <ginne.zhang@mail.utoronto.ca>
2023-11-19 22:52:16 -0500

    updated All features plot

--- A2/Allfeatures_RDplot_Time.m
+++ A2/Allfeatures_RDplot_Time.m
@@ -8,17 +8,36 @@ height = 288;
 blockSize = 16; % Fixed block size
 r = 4; % Fixed search range
 I_Period = 8; % Fixed I_Period
-QPs = [1,2,3,4,5,6,7,8,9,10,11]; % Array of QP values
+QPs = [1,4,7,10]; % Array of QP values
 
 % Initialize arrays for feature comparison
 psnrValuesAllFeatures = zeros(length(QPs), 1);
 psnrValuesNoFeatures = zeros(length(QPs), 1);
+psnrValuesNRF = zeros(length(QPs), 1);
+psnrValuesVBS = zeros(length(QPs), 1);
+psnrValuesFME = zeros(length(QPs), 1);
+psnrValuesFastME = zeros(length(QPs), 1);
+
 encTimesAllFeatures = zeros(size(psnrValuesAllFeatures));
 encTimesNoFeatures = zeros(size(psnrValuesNoFeatures));
+encTimesNRF = zeros(size(psnrValuesNRF));
+encTimesVBS = zeros(size(psnrValuesVBS));
+encTimesFME = zeros(size(psnrValuesFME));
+encTimesFastME = zeros(size(psnrValuesFastME));
+
 decTimesAllFeatures = zeros(size(psnrValuesAllFeatures));
 decTimesNoFeatures = zeros(size(psnrValuesNoFeatures));
+decTimesNRF = zeros(size(psnrValuesNRF));
+decTimesVBS = zeros(size(psnrValuesVBS));
+decTimesFME = zeros(size(psnrValuesFME));
+decTimesFastME = zeros(size(psnrValuesFastME));
+
 bitSizesAllFeatures = zeros(size(psnrValuesAllFeatures));
 bitSizesNoFeatures = zeros(size(psnrValuesNoFeatures));
+bitSizesNRF = zeros(size(psnrValuesNRF));
+bitSizesVBS = zeros(size(psnrValuesVBS));
+bitSizesFME = zeros(size(psnrValuesFME));
+bitSizesFastME = zeros(size(psnrValuesFastME));
 
 for q = 1:length(QPs)
     QP = QPs(q);
@@ -37,31 +56,56 @@ for q = 1:length(QPs)
     FastME = true;
     nRefFrames = 4;
     [bitSizesAllFeatures(q), encTimesAllFeatures(q), decTimesAllFeatures(q), psnrValuesAllFeatures(q)] = runTest(yuvInputFileName, nFrame, width, height, blockSize, r, QP, I_Period, nRefFrames, VBSEnable, FMEEnable, FastME, false, Lambda);
+
+    VBSEnable = true;
+    FMEEnable = false;
+    FastME = false;
+    nRefFrames = 1;
+    [bitSizesVBS(q), encTimesVBS(q), decTimesVBS(q), psnrValuesVBS(q)] = runTest(yuvInputFileName, nFrame, width, height, blockSize, r, QP, I_Period, nRefFrames, VBSEnable, FMEEnable, FastME, false, Lambda);
+
+    VBSEnable = false;
+    FMEEnable = true;
+    FastME = false;
+    nRefFrames = 1;
+    [bitSizesFastME(q), encTimesFastME(q), decTimesFastME(q), psnrValuesFastME(q)] = runTest(yuvInputFileName, nFrame, width, height, blockSize, r, QP, I_Period, nRefFrames, VBSEnable, FMEEnable, FastME, false, Lambda);
+
+    VBSEnable = false;
+    FMEEnable = true;
+    FastME = false;
+    nRefFrames = 1;
+    [bitSizesFME(q), encTimesFastME(q), decTimesFastME(q), psnrValuesFastME(q)] = runTest(yuvInputFileName, nFrame, width, height, blockSize, r, QP, I_Period, nRefFrames, VBSEnable, FMEEnable, FastME, false, Lambda);
+
+    VBSEnable = false;
+    FMEEnable = false;
+    FastME = false;
+    nRefFrames = 4;
+    [bitSizesNRF(q), encTimesNRF(q), decTimesNRF(q), psnrValuesNRF(q)] = runTest(yuvInputFileName, nFrame, width, height, blockSize, r, QP, I_Period, nRefFrames, VBSEnable, FMEEnable, FastME, false, Lambda);
+
 end
 
 % Plotting the RD Curve for Feature Comparison
 figure;
-plot(bitSizesNoFeatures, psnrValuesNoFeatures, 'b-o', bitSizesAllFeatures, psnrValuesAllFeatures, 'r-*');
-title('Rate-Distortion Curve: No Features vs All Features');
+plot(bitSizesNoFeatures, psnrValuesNoFeatures, 'r-', bitSizesAllFeatures, psnrValuesAllFeatures, 'c-', bitSizesVBS, psnrValuesVBS, 'g-', bitSizesNRF, psnrValuesNRF, 'b-', bitSizesFME, psnrValuesFME, 'm-', bitSizesFastME, psnrValuesFastME, 'y-');
+title('Rate-Distortion Curve');
 xlabel('Total Size in Bits');
 ylabel('PSNR (dB)');
-legend('No Features', 'All Features', 'Location', 'southeast');
+legend('No Features', 'All Features', 'VBS', 'nRefFrames', 'FME', 'FastME', 'Location', 'southeast');
 
 % Plotting the Encoding Times for Feature Comparison
 figure;
-plot(QPs, encTimesNoFeatures, 'b-o', QPs, encTimesAllFeatures, 'r-*');
-title('Encoding Times: No Features vs All Features');
+plot(QPs, encTimesNoFeatures, 'r-', QPs, encTimesAllFeatures, 'c-', QPs, encTimesVBS, 'g-', QPs, encTimesNRF, 'b-', QPs, encTimesFME, 'm-', QPs, encTimesFastME, 'y-');
+title('Encoding Times');
 xlabel('QP');
 ylabel('Time (s)');
-legend('No Features', 'All Features');
+legend('No Features', 'All Features', 'VBS', 'nRefFrames', 'FME', 'FastME', 'Location', 'southeast');
 
 % Plotting the Decoding Times for Feature Comparison
 figure;
-plot(QPs, decTimesNoFeatures, 'b-o', QPs, decTimesAllFeatures, 'r-*');
-title('Decoding Times: No Features vs All Features');
+plot(QPs, decTimesNoFeatures, 'r-', QPs, decTimesAllFeatures, 'c-', QPs, decTimesVBS, 'g-', QPs, decTimesNRF, 'b-', QPs, decTimesFME, 'm-', QPs, decTimesFastME, 'y-');
+title('Decoding Times');
 xlabel('QP');
 ylabel('Time (s)');
-legend('No Features', 'All Features');
+legend('No Features', 'All Features', 'VBS', 'nRefFrames', 'FME', 'FastME', 'Location', 'southeast');
 
 function [bitSize, encTime, decTime, psnrValue] = runTest(yuvInputFileName, nFrame, width, height, blockSize, r, QP, I_Period, nRefFrames, VBSEnable, FMEEnable, FastME, visualizeVBS, Lambda)
     % Run encoder
@@ -87,11 +131,7 @@ function [bitSize, encTime, decTime, psnrValue] = runTest(yuvInputFileName, nFra
     % Calculate PSNR
     YOutput = importYOnly(['DecoderOutput' filesep 'outputYUV.yuv'], width, height, nFrame);
     [YOriginal, ~, ~] = importYUV(yuvInputFileName, width, height, nFrame);
-    psnrValue = 0;
-    for i = 1:nFrame
-        psnrValue = psnrValue + psnr(YOutput(:,:,i), YOriginal(:,:,i));
-    end
-    psnrValue = psnrValue / nFrame;
+    psnrValue = psnr(YOutput, YOriginal);
 end
 
 function totalBits = calculateBitSize(QTCCoeffs, MDiffs, splits, nFrame)
